62fe32cc5fd20c502b2a0fa4c45ede139a2541d2
teacher05 <aeshemet@gmail.com>
2024-11-21 13:37:55 +0300

    commit

---
+++ studyProject/src/Main.java
@@ -0,0 +1,15 @@
+import lesson4.Lesson4;
+
+import static lesson4.Lesson4.str;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+      System.out.println(str.length());
+
+      System.out.println(str.charAt(4));
+
+      System.out.println(str.substring(3, str.length()));
+    }
+}
---
+++ studyProject/src/lesson4/Lesson4.java
@@ -0,0 +1,8 @@
+package lesson4;
+
+public class Lesson4 {
+
+    static public String str = "I study Java";
+
+
+}
